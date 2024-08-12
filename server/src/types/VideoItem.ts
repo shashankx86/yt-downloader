3a35e612a6c6fc1025d45d6433217bf9d133bf87
shashankx86 <shashankspis20@gmail.com>
2024-08-12 07:49:09 +0530

    Add some typings for the YT Playlist API response

---
+++ server/src/types/VideoItem.ts
@@ -0,0 +1,45 @@
+export interface YTVideoItem {
+    kind:    string;
+    etag:    string;
+    id:      string;
+    snippet: Snippet;
+}
+
+export interface Snippet {
+    publishedAt:            Date;
+    channelId:              string;
+    title:                  string;
+    description:            string;
+    thumbnails:             Thumbnails;
+    channelTitle:           string;
+    playlistId:             string;
+    position:               number;
+    resourceId:             ResourceID;
+    videoOwnerChannelTitle: string;
+    videoOwnerChannelId:    string;
+}
+
+export interface ResourceID {
+    kind:    string;
+    videoId: string;
+}
+
+export interface Thumbnails {
+    default: Default;
+    medium:  Default;
+    high:    Default;
+}
+
+export interface Default {
+    url:    string;
+    width:  number;
+    height: number;
+}
+
+export interface VideoItemData {
+    videoId: string;
+    title: string;
+    description: string;
+    publishedAt: Date;
+    thumbnails: Thumbnails;
+}
