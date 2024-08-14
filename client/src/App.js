5b8ae99a2b5da4426e0a9f304a2e54627e68102a
shashankx86 <shashankspis20@gmail.com>
2024-08-15 04:10:17 +0530

    Socket context

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,35 @@ import Playlist from './components/pages/Playlist';
 import Navbar from './components/Navbar';
 import React, { useState, useEffect } from 'react';
 import socket from './services/socket';
-
+import SocketContext from './context/SocketContext';
 
 function App() {
 
+  const [connected, setConnected] = useState(false);
+
   useEffect(() => {
+    socket.on('connect', () => setConnected(true));
+    socket.on('disconnect', () => setConnected(false));
+
     return () => {
       socket.off('connect');
       socket.off('disconnect');
-      socket.off('pong');
     };
-
   }, [])
 
   return (
     <div className="App">
+      <SocketContext.Provider value={{
+        isConnected: connected, 
+        connection: socket, 
+        id: socket.id
+      }}>
         <Navbar />
         <Routes>
           <Route path="/" element={<Video />} />
           <Route path="playlist" element={<Playlist />} />
         </Routes>
+      </SocketContext.Provider>
     </div>
   );
 }
